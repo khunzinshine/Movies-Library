48012369022b764ecd7ab9ab43851106d45cc578
Khun Zin Shine <khunzinshine@ayainnovation.com>
2021-07-28 14:40:43 +0630

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import React, { Component } from "react";
 import "./App.css";
 import "./components/movies";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
+import Rental from "./components/rentals";
+import NotFound from "./components/notFound";
+import Customer from "./components/customers";
+import NavBar from "./components/navBar";
 
-function App() {
-  return (
-    <main className="container">
-      <Movies />
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies" component={Movies} />
+            <Route path="/movie-form" component={MovieForm} />
+            <Route path="/rentals" component={Rental} />
+            <Route path="/customers" component={Customer} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
