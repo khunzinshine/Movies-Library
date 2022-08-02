fcc492ee0f779c6e89a8d6d90fe706a980800e53
khunzinshine <khunzinshine@ayainnovation.com>
2022-08-02 22:37:27 +0630

    modify header

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          Vidly
+          Khun's
         </Link>
         <button
           className="navbar-toggler"
